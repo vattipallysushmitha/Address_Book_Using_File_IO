8d4acaa0d7d86fefe722db5fbaaf985d74be98fd
sushmitha <vengalreddysushmitha@gmail.com>
2022-09-17 11:05:48 -0700

    [SUSHMITHA]ADDED:Read and Write the AddressBook with Person Name

---
+++ AddressBookIO/src/main/java/com/bridzelab/AddressBookIO/AddressBookMain.java
@@ -0,0 +1,24 @@
+package com.bridzelab.AddressBookIO;
+
+import java.util.*;
+
+public class AddressBookMain{
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("WELCOME TO ADDREES BOOK PROGRAM");
+        HashMap<String, AddressBook> map = new HashMap<>();//Creating HashMap
+        System.out.println("Enter Number of address book you want to add");
+        int count = scan.nextInt();
+        int i = 0;
+        while (i < count) {
+            System.out.println("Enter book name");
+            String bookName = scan.next();
+            AddressBook addressBook = new AddressBook();
+            addressBook.operation();
+            map.put(bookName, addressBook);
+            i++;
+        }
+        System.out.println(map);
+    }
+}
